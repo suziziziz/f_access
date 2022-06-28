4cbc3466f4ae45852e8b78bf18d7e20502a54b0e
Suziziziz <lyperodrigues03@gmail.com>
2022-06-28 00:47:47 -0300

    Some changes, including bottom bar modify & scroll effect

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       routes: {
-        '/': (context) => const Home(),
+        '/': (context) => Home(),
       },
     );
   }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math.dart' as vector;
 import 'package:f_access/typo/typo_colors.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final ScrollController _scrollController = ScrollController();
+  double _scrollPosition = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +24,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        toolbarHeight: 248,
+        toolbarHeight: (248 - _scrollPosition).clamp(64, 248),
         title: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 16),
@@ -25,20 +38,47 @@ class Home extends StatelessWidget {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(16),
         child: Container(
-          color: Colors.white,
-          height: 64,
+          constraints: const BoxConstraints(maxHeight: 96),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(width: 16, color: Colors.white),
+              bottom: BorderSide(width: 16, color: Colors.white),
+            ),
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              const Text("Hey Sarah!", style: TextStyle(fontSize: 20)),
-              Container(
-                constraints: const BoxConstraints(minHeight: 64, minWidth: 64),
+              const Text("Hey Sarah!",
+                  style: TextStyle(fontSize: 20, color: TypoColors.black2)),
+              const SizedBox(width: 16),
+              Stack(
                 alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(32),
-                  color: Colors.red,
-                ),
-                child: const Text("asd"),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: TypoColors.gradientMain,
+                        borderRadius: BorderRadius.circular(32)),
+                    constraints: const BoxConstraints(
+                      minHeight: 64,
+                      minWidth: 64,
+                    ),
+                  ),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(32),
+                        color: Colors.black),
+                  ),
+                  ShaderMask(
+                    blendMode: BlendMode.srcIn,
+                    shaderCallback: (bounds) =>
+                        TypoColors.gradientMain.createShader(bounds),
+                    child: const Icon(Icons.mic_rounded, size: 32),
+                  ),
+                ],
               ),
             ],
           ),
@@ -46,48 +86,92 @@ class Home extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics(),
-          ),
-          children: [
-            Wrap(
-              runSpacing: 16,
-              children: const [
-                HelpButton(
-                  icon: Icons.security_rounded,
-                  title: "CTEPO",
-                  description: "Pedir ajuda da policia.",
-                ),
-                HelpButton(
-                  icon: Icons.emergency_rounded,
-                  title: "CARGY",
-                  description: "Chamar ambulância.",
-                ),
-                HelpButton(
-                  icon: Icons.fire_truck_rounded,
-                  title: "CFIRE",
-                  description: "Chamar corpo de bombeiros.",
-                ),
-                HelpButton(
-                  icon: Icons.directions_bus_rounded,
-                  title: "POBUS",
-                  description: "Checar se o ônibus está chegando.",
-                ),
-                HelpButton(
-                  icon: Icons.compare_rounded,
-                  title: "OBJID",
-                  description: "Identificar um objeto com a câmera.",
-                ),
-                HelpButton(
-                  icon: Icons.my_location_rounded,
-                  title: "LATIO",
-                  description:
-                      "Saber aonde ir, onde está, locais próximos, etc.",
-                ),
-              ],
+        child: NotificationListener<ScrollUpdateNotification>(
+          onNotification: (notification) {
+            setState(() {
+              _scrollPosition = notification.metrics.pixels;
+            });
+            return true;
+          },
+          child: ListView(
+            controller: _scrollController,
+            physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics(),
             ),
-          ],
+            children: [
+              Container(
+                color: Colors.redAccent,
+                height: _scrollPosition.clamp(0, 248 - 64),
+              ),
+              Wrap(
+                runSpacing: 16,
+                children: const [
+                  HelpButton(
+                    icon: Icons.security_rounded,
+                    title: "CTEPO",
+                    description: "Pedir ajuda da policia.",
+                  ),
+                  HelpButton(
+                    icon: Icons.emergency_rounded,
+                    title: "CARGY",
+                    description: "Chamar ambulância.",
+                  ),
+                  HelpButton(
+                    icon: Icons.fire_truck_rounded,
+                    title: "CFIRE",
+                    description: "Chamar corpo de bombeiros.",
+                  ),
+                  HelpButton(
+                    icon: Icons.directions_bus_rounded,
+                    title: "POBUS",
+                    description: "Checar se o ônibus está chegando.",
+                  ),
+                  HelpButton(
+                    icon: Icons.compare_rounded,
+                    title: "OBJID",
+                    description: "Identificar um objeto com a câmera.",
+                  ),
+                  HelpButton(
+                    icon: Icons.my_location_rounded,
+                    title: "LATIO",
+                    description:
+                        "Saber aonde ir, onde está, locais próximos, etc.",
+                  ),
+                  HelpButton(
+                    icon: Icons.security_rounded,
+                    title: "CTEPO",
+                    description: "Pedir ajuda da policia.",
+                  ),
+                  HelpButton(
+                    icon: Icons.emergency_rounded,
+                    title: "CARGY",
+                    description: "Chamar ambulância.",
+                  ),
+                  HelpButton(
+                    icon: Icons.fire_truck_rounded,
+                    title: "CFIRE",
+                    description: "Chamar corpo de bombeiros.",
+                  ),
+                  HelpButton(
+                    icon: Icons.directions_bus_rounded,
+                    title: "POBUS",
+                    description: "Checar se o ônibus está chegando.",
+                  ),
+                  HelpButton(
+                    icon: Icons.compare_rounded,
+                    title: "OBJID",
+                    description: "Identificar um objeto com a câmera.",
+                  ),
+                  HelpButton(
+                    icon: Icons.my_location_rounded,
+                    title: "LATIO",
+                    description:
+                        "Saber aonde ir, onde está, locais próximos, etc.",
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
